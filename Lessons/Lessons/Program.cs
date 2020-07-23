b2c346f392eb775a9d3f0d5f6acceb8e5308afac
Mixa <dr.migus@yandex.ru>
2020-07-24 02:46:49 +0300

    Точки на экране через конструктор и инкапсуляцию

---
+++ Lessons/Lessons/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(4, 5, '@');
+            p1.Draw();
+
+            Point p2 = new Point(2, 1, '^');
+            p2.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
+
