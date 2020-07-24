2ef417523bedde8c2145ce7daddb9160ef0fdb1f
Mixa <dr.migus@yandex.ru>
2020-07-24 03:35:14 +0300

    Примеры команд со списком

--- Lessons/Lessons/Program.cs
+++ Lessons/Lessons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lessons
 {
@@ -11,6 +12,23 @@ namespace Lessons
 
             Point p2 = new Point(2, 1, '^');
             p2.Draw();
+
+            Point p3 = new Point(7, 10, '!');
+            p3.Draw();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+/*  Пример цикла вывода списка на экран
+            foreach (Point i in pList)
+            {
+                Console.WriteLine(i);
+            }
+
+    int x = numlist[0] - задать переменной х значение в данной ячейке списка
+    pList.RemoveAt(0) - удалить ячейку списка под номером 
+*/
 
             Console.ReadLine();
         }
